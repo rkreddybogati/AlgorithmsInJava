39001ff38d69c1cf354b86903cf4cb45862695aa
Ramakrishna Reddy <rkreddy.bogati@gmail.com>
2017-07-19 13:41:13 +0530

    StackUsingLinkedList complete code.

--- AlgorithmsinJava/src/main/java/com/rk/stack/StackUsingLinkedList.java
+++ AlgorithmsinJava/src/main/java/com/rk/stack/StackUsingLinkedList.java
@@ -1,6 +1,7 @@
 package com.rk.stack;
 
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /** Stack is basically LIFO - Last In First Out
  * Created by rbhogati on 17/7/17.
@@ -57,6 +58,7 @@ public class StackUsingLinkedList {
         }
         current = head;
         head = head.getNextLink();
+        size--;
         return current.getData();
     }
 
@@ -67,6 +69,63 @@ public class StackUsingLinkedList {
         return head.getData();
     }
 
+    public void display(){
+        System.out.println("Stack= ");
+        if (size == 0){
+            System.out.print("Empty\n");
+            return;
+        }
+        Node ptr = head;
+        while (ptr != null){
+            System.out.print(ptr.getData()+ " ");
+            ptr = ptr.getNextLink();
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        StackUsingLinkedList st = new StackUsingLinkedList();
+
+        /* Perform Queue Operations */
+        System.out.println("Linked Stack Test\n");
+
+        char yesOrNo;
+        do {
+            System.out.println("\nStack Operations");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Check empty");
+            System.out.println("5. Size");
+            int choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter data to push");
+                    st.push(scan.nextInt());
+                    break;
+                case 2:
+                    st.pop();
+                    break;
+                case 3:
+                    System.out.println("Peek: " + st.peek());
+                    break;
+                case 4:
+                    System.out.println("IsEMpty = "+ st.isEmpty());
+                    break;
+                case 5:
+                    System.out.println("Size = "+ st.getSize());
+                    break;
+                default:
+                    System.out.println("Wrong Entry \n ");
+                    break;
+            }
+
+            st.display();
+            System.out.println("\n Do you want to continue (Type y or n). ");
+            yesOrNo = scan.next().charAt(0);
+        } while ( yesOrNo == 'y' || yesOrNo == 'Y' );
+    }
 
 }
